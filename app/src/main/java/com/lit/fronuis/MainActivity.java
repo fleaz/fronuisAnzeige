06cb9a98dc33ad9d8fea42f6b9b267db4a8e113a
Fabian <1803fabian@gmail.com>
2021-05-28 17:34:34 +0200

    Initial commit

---
+++ app/src/main/java/com/lit/fronuis/MainActivity.java
@@ -0,0 +1,53 @@
+package com.lit.fronuis;
+
+import androidx.appcompat.app.AppCompatActivity;
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hideSystemUI();
+        GeckoView view = findViewById(R.id.geckoview);
+        GeckoSession session = new GeckoSession();
+        GeckoRuntime runtime = GeckoRuntime.create(this);
+
+        session.open(runtime);
+        view.setSession(session);
+        session.loadUri("http://192.168.178.58/#/dashboard");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        hideSystemUI();
+    }
+
+    private void hideSystemUI()
+    {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+}
