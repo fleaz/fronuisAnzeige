717430444890851c85a36981a8fb6482bf4f8ca9
Fabian <1803fabian@gmail.com>
2021-08-30 23:13:59 +0200

    changed webview from gecko to android webkit cropped website added weatherpanel

--- app/src/main/java/com/lit/fronuis/MainActivity.java
+++ app/src/main/java/com/lit/fronuis/MainActivity.java
@@ -15,6 +15,7 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity
 {
     WebView webView;
+    WebView webWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,8 +23,13 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        webView = new WebView(MainActivity.this);
-        setContentView(webView);
+        //webView = new WebView(MainActivity.this);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.web);
+        webWeather = findViewById(R.id.webv);
+        webWeather.getSettings().setJavaScriptEnabled(true);
+        webWeather.setWebViewClient(new WebViewClient());
+        webWeather.loadUrl("file:///android_asset/weather.html");
         //setContentView(R.layout.activity_main);
         //hideSystemUI();
         //webv = findViewById(R.id.webv);
@@ -31,6 +37,8 @@ public class MainActivity extends AppCompatActivity
         webView.setWebViewClient(client);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
+        String newUA= "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.4) Gecko/20100101 Firefox/4.0";
+        //webView.getSettings().setUserAgentString(newUA);
         webView.loadUrl("http://192.168.178.58/#/dashboard");
 
         final Handler handler = new Handler();
@@ -38,8 +46,19 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void run() {
                 webView.evaluateJavascript("document.getElementsByTagName('site-header')[0].remove();", null);
+                webView.evaluateJavascript("document.getElementsByTagName('main-menu')[0].remove()", null);
+                webView.evaluateJavascript("document.getElementsByTagName('mat-tab-header')[0].remove()", null);
+                webView.evaluateJavascript("document.getElementsByClassName('ng-tns-c331-9 card-inner cardwrapper-col-sm-5 cardwrapper-col-md-6 cardwrapper-col-md-offset-0 ng-trigger ng-trigger-triggerFade ng-star-inserted')[0].remove()", null);
+                webView.evaluateJavascript("document.getElementsByClassName('ng-tns-c331-9 card-inner cardwrapper-col-sm-12 cardwrapper-col-md-6 cardwrapper-col-md-offset-0 ng-trigger ng-trigger-triggerFade ng-star-inserted')[0].remove()", null);
+                webView.evaluateJavascript("document.getElementsByClassName('container-fluid app-content ng-tns-c48-0')[0].style.marginLeft = 0", null);
+                webView.evaluateJavascript("document.getElementsByClassName('beneath-header ng-tns-c48-0')[0].style.top = 0", null);
+                webView.evaluateJavascript("document.getElementsByClassName('container-fluid app-content ng-tns-c48-0')[0].style.paddingLeft = 0", null);
+                webView.evaluateJavascript("document.getElementsByClassName('mat-card-title ng-tns-c331-9 ng-star-inserted')[0].remove()", null);
+                webView.evaluateJavascript("document.getElementsByClassName('container-fluid app-content ng-tns-c48-0')[0].style.backgroundColor = \"#000000\"", null);
+                webView.evaluateJavascript("document.getElementsByClassName('mat-card mat-focus-indicator ng-tns-c331-9')[0].style.backgroundColor = \"#000000\"", null);
+                webView.scrollBy(0, 120);
             }
-        }, 10000);
+        }, 20000);
 
     }
 
